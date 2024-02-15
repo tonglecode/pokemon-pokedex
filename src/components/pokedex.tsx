1a3b4ef427c5c6f1a9920fbf4cd23f720768d3f1
dofresh <dofresh.dev@gmail.com>
2024-02-15 14:57:48 +0900

    feat: 인피니티 스크롤

--- src/components/pokedex.tsx
+++ src/components/pokedex.tsx
@@ -1,16 +1,18 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import "./pokedex.css";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 interface Props {}
 
 const Pokedex: React.FC<Props> = ({}) => {
   const [pokemonData, setPokemonData] = useState<any[]>([]);
-
+  const [currentPage, setCurrentPage] = useState(1);
+  const pokemonPerPage = 12;
   useEffect(() => {
     const fetchData = async () => {
       const allPokemonData = [];
-      for (let i = 1; i <= 12; i++) {
+      for (let i = 1; i <= currentPage * pokemonPerPage; i++) {
         const response = await axios.get(
           `https://pokeapi.co/api/v2/pokemon/${i}`
         );
@@ -26,24 +28,34 @@ const Pokedex: React.FC<Props> = ({}) => {
     };
 
     fetchData();
-    console.log(pokemonData);
-  }, []);
+  }, [currentPage]);
 
-  const renderPokemonList = () => {
-    return pokemonData.map((pokemon) => (
-      <div key={pokemon.id} className="pokemon">
-        <img src={pokemon.sprites.front_default} alt={pokemon.korean_name} />
-        <div>
-          <p>{pokemon.korean_name}</p>
-          <p className="eng">{pokemon.name}</p>
-          <br />
-          <p>ID: {pokemon.id}</p>
-        </div>
-      </div>
-    ));
+  const fetchMoreData = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
   };
 
-  return <div className="container">{renderPokemonList()}</div>;
+  return (
+    <InfiniteScroll
+      dataLength={pokemonData.length}
+      next={fetchMoreData}
+      hasMore={currentPage * pokemonPerPage < 151}
+      loader={<h4>Loading...</h4>}
+      endMessage={<p>All Pokémon have been loaded</p>}
+      className="container"
+    >
+      {pokemonData.map((pokemon) => (
+        <div key={pokemon.id} className="pokemon">
+          <img src={pokemon.sprites.front_default} alt={pokemon.korean_name} />
+          <div>
+            <p>{pokemon.korean_name}</p>
+            <p className="eng">{pokemon.name}</p>
+            <br />
+            <p>ID: {pokemon.id}</p>
+          </div>
+        </div>
+      ))}
+    </InfiniteScroll>
+  );
 };
 
 export default Pokedex;
